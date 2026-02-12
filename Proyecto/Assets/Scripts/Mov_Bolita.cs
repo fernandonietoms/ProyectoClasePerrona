d378951e41a6a084232f2615a569e24a4e8f63f2
Matjune <juanmondmat22@gmail.com>
2026-02-11 21:10:33 -0600

    Add Platform Prefab
    First map creation, 4 platforms and jump movement adding for Bolita.

--- Proyecto/Assets/Scripts/Mov_Bolita.cs
+++ Proyecto/Assets/Scripts/Mov_Bolita.cs
@@ -11,84 +11,103 @@ public class NewMov_Bolita : MonoBehaviour
 
     [SerializeField, Range(0.001f,0.1f)]
     float speed = 0.01f;
+    [SerializeField, Range(5.0f, 15.0f)]
+    float jump = 10f;
+    [SerializeField]
+    float jumpOffset = 0.5f;
+
+    [SerializeField] Rigidbody rigidBodyPlayer;
 
     // Update is called once per frame
     void Update()
     {
-        // Move Forward Instruction
+        // Move Forward Instruction ----------------------------------
 
         // If the W key is pressed down...
         if (Input.GetKeyDown(KeyCode.W))
-        {        
+        {
             // Enable Moving
             moveForward = true;
         }
         // If the W key is stopped pressed...
         if (Input.GetKeyUp(KeyCode.W))
-        {        
+        {
             // Disable moving
             moveForward = false;
         }
         if (moveForward)
         {
-            GetComponent<Rigidbody>().AddForce(Vector3.forward * speed, ForceMode.Impulse);
+            rigidBodyPlayer.AddForce(Vector3.forward * speed, ForceMode.Impulse);
         }
 
-        // Move Backward Instruction
+        // Move Backward Instruction ---------------------------------
 
         // If the S key is pressed down...
         if (Input.GetKeyDown(KeyCode.S))
-        {        
+        {
             // Enable Moving
             moveBackward = true;
         }
         // If the A key is stopped pressed...
         if (Input.GetKeyUp(KeyCode.S))
-        {        
+        {
             // Disable moving
             moveBackward = false;
         }
         if (moveBackward)
         {
-            GetComponent<Rigidbody>().AddForce(Vector3.back * speed, ForceMode.Impulse);
+            rigidBodyPlayer.AddForce(Vector3.back * speed, ForceMode.Impulse);
         }
 
-        // Move Left Instruction
+        // Move Left Instruction -------------------------------
 
         // If the A key is pressed down...
         if (Input.GetKeyDown(KeyCode.A))
-        {        
+        {
             // Enable Moving
             moveLeft = true;
         }
         // If the A key is stopped pressed...
         if (Input.GetKeyUp(KeyCode.A))
-        {        
+        {
             // Disable moving
             moveLeft = false;
         }
         if (moveLeft)
         {
-            GetComponent<Rigidbody>().AddForce(Vector3.left * speed, ForceMode.Impulse);
+            rigidBodyPlayer.AddForce(Vector3.left * speed, ForceMode.Impulse);
         }
 
-        // Move Right Instruction
+        // Move Right Instruction ------------------------------
 
         // If the D key is pressed down...
         if (Input.GetKeyDown(KeyCode.D))
-        {        
+        {
             // Enable Moving
             moveRight = true;
         }
         // If the D key is stopped pressed...
         if (Input.GetKeyUp(KeyCode.D))
-        {        
+        {
             // Disable moving
             moveRight = false;
         }
         if (moveRight)
         {
-            GetComponent<Rigidbody>().AddForce(Vector3.right * speed, ForceMode.Impulse);
+            rigidBodyPlayer.AddForce(Vector3.right * speed, ForceMode.Impulse);
         }
+
+        // Move Jump Instruction ------------------------------
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if ((rigidBodyPlayer.linearVelocity.y <= jumpOffset) &&
+              (rigidBodyPlayer.linearVelocity.y >= -jumpOffset))
+            {
+                // Jump 
+                rigidBodyPlayer.AddForce(Vector3.up * jump, ForceMode.Impulse);
+            }
+        }
+
     }
 }
