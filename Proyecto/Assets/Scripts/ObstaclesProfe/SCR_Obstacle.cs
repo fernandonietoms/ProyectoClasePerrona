37f617a74aabcdf6006d13d47ad4f4fe76d3c671
Fernando Nieto Morales <fernando.nietoms@gmail.com>
2026-02-17 22:05:56 -0600

    Add Coroutine to move Obstacles

--- Proyecto/Assets/Scripts/ObstaclesProfe/SCR_Obstacle.cs
+++ Proyecto/Assets/Scripts/ObstaclesProfe/SCR_Obstacle.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -7,9 +8,58 @@ using UnityEngine;
 [RequireComponent(typeof(MeshCollider))]
 public class SCR_Obstacle : MonoBehaviour
 {
+    [Header("Obstacle Components")]
+    [SerializeField] bool canMove = false;
+    [SerializeField, Range(0, Mathf.Infinity)] float obstacleSpeed = 1.0f;
+    [SerializeField] float waitingTime = 0.0f;
+    [SerializeField] Transform startLocation;
+    [SerializeField] Transform endLocation;
+    bool isMovingToEnd = true;
+    Coroutine waitingTimeCoroutine;
+
     protected virtual void OnCollisionEnter(UnityEngine.Collision collision)
     {
         Debug.Log("Obstacle Detected");
-        Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        if (canMove)
+        {
+            transform.position = startLocation.position;
+        }
+    }
+
+    private void Update()
+    {
+        if (canMove)
+        {
+            if (isMovingToEnd)
+            {
+                if (Vector3.Distance(transform.position, endLocation.position) < 0.01f)
+                {
+                    if (waitingTimeCoroutine == null)
+                        waitingTimeCoroutine = StartCoroutine(WaitingTimer());
+                }
+                transform.position = Vector3.MoveTowards(transform.position, endLocation.position, Time.deltaTime * obstacleSpeed);
+            }
+            else
+            {
+                if (Vector3.Distance(transform.position, startLocation.position) < 0.01f)
+                {
+                    if (waitingTimeCoroutine == null)
+                        waitingTimeCoroutine = StartCoroutine(WaitingTimer());
+                }
+                transform.position = Vector3.MoveTowards(transform.position, startLocation.position, Time.deltaTime * obstacleSpeed);
+            }
+        }
+    }
+
+    IEnumerator WaitingTimer()
+    {
+        yield return new WaitForSeconds(waitingTime);
+        isMovingToEnd = !isMovingToEnd;
+        StopCoroutine(waitingTimeCoroutine);
+        waitingTimeCoroutine = null;
     }
 }
